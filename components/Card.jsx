c82b5cf9bfdde7f9091c25236c0f9f3c78319a34
romeokany187 <romeokany187@gmail.com>
2024-10-08 02:31:06 +0100

    modifications

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,9 +5,9 @@ import React from "react";
 const Card = ({ img, head, sub, href, alt }) => {
   return (
     <div
-      className=" lg:w-[15rem] w-[22rem] h-[23rem] p-3 PX"
+      className=" lg:w-[15rem] w-[22rem] h-[27rem] p-3 PX"
     >
-      <div className="p-8 flex flex-col gap-3 text-center justify-center items-center rounded-xl shadow-md h-[80%] ">
+      <div className="p-8 flex flex-col gap-8 text-center justify-center items-center rounded-xl shadow-md h-[85%] ">
         <Link href={href} className="">
           <Image
             src={img}
